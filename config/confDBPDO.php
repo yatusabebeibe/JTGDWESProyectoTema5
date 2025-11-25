383119e3a372711926e2f164f7350850089e7d69
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2025-11-26 00:17:37 +0100

    feat: añadir mostrar codigo del archivo de config y mostrar tanto desarrollo como explotacion

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -6,21 +6,33 @@
         https://mclibre.org/consultar/php/lecciones/php-constantes.html
 */
 
-const DBHost = "10.199.11.252";
+const DBHost = "10.199.11.252"; // desarrollo
+// const DBHost = "localhost"; // explotación
 
 // admin
 const DBUserRoot = "adminsql";
-const DBPassRoot = "paso";
+const DBPassRoot = "paso"; // desarrollo
+// const DBPassRoot = "!Paso1234x"; // explotación
 const DSNRoot = "mysql:host=".DBHost;
 
 // usuario
 const DBName = "DBJTGDWESProyectoTema5";
 const DBUser = "userJTGDWESProyectoTema5";
-const DBPass = "paso";
+const DBPass = "paso"; // desarrollo
+// const DBPass = "!Paso1234x"; // explotación
 const DSN = "mysql:host=".DBHost.";dbname=".DBName;
 
 // Array con el nombre de las columnas que vamos a seleccionar
-const aColumnas = [
+const aColumnasUsuario = [
+    "Codigo" => "T01_CodUsuario",
+    "Password" => "T01_Password",
+    "Descripcion" => "T01_DescUsuario",
+    "NumConexiones" => "T01_NumConexiones",
+    "UltimaConexion" => "T01_FechaHoraUltimaConexion",
+    "Perfil" => "T01_Perfil",
+    "Imagen" => "T01_ImagenUsuario"
+];
+const aColumnasDepartamento = [
     "Codigo" => "T02_CodDepartamento",
     "Descripcion" => "T02_DescDepartamento",
     "Volumen" => "T02_VolumenDeNegocio",
